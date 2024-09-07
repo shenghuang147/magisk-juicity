a6ce24252c44730991e45df35d91d598a416774b
SHENG <shenghuang147@gmail.com>
2024-09-08 05:01:21 +0800

    fix: waiting for the system to start up before booting

--- service.sh
+++ service.sh
@@ -4,5 +4,11 @@ MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
 if [ ! -f "$MODDIR"/disable ]; then
-    $MODDIR/juicity.init start
+    # Wait for the system to boot up before launching juicity
+    until [ $(getprop sys.boot_completed) -eq 1 ]; do
+        sleep 3
+        $MODDIR/juicity.init start
+        break
+    done
 fi
+
